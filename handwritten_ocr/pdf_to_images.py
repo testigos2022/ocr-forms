0bf9ed642700c5e3310ad92a420c4a97979a91d6
Tilo Himmelsbach <tilo.himmelsbach@iais.fraunhofer.de>
2022-01-23 08:27:03 +0100

    pdf_to_images.py

---
+++ handwritten_ocr/pdf_to_images.py
@@ -0,0 +1,15 @@
+import os
+from pathlib import Path
+
+from pdf2image import convert_from_path
+from tqdm import tqdm
+
+if __name__ == '__main__':
+#    pip install pdf2image
+    data_path = os.environ['DATA_PATH']
+    pdf_file=f"{data_path}/esc_cong_2018/data/esc_cong_2018_archivos_divulgacion_AGE_XXX_2_01_004_XXX_XX_XX_X_1052_F_49.pdf"
+    pages = convert_from_path(pdf_file, 200)
+    output_dir= f"{data_path}/esc_cong_2018/jpegs"
+    os.makedirs(output_dir,exist_ok=True)
+    for k,page in tqdm(enumerate(pages)):
+        page.save(f'{output_dir}/{Path(pdf_file).name}-{k}.jpg', 'JPEG')
